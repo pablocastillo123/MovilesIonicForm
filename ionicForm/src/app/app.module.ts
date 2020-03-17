ff48a4040df6d153e3253a0aaae4752da42aa25a
Luis Villalobos <luisnazario2310@gmail.com>
2020-03-17 18:41:30 -0400

    Services

--- ionicForm/src/app/app.module.ts
+++ ionicForm/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { } from '@angular/fire'
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
